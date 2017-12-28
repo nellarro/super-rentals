672120870873450314059a87e4f7ca3089e57215
Jenell Pizarro <nellarro@gmail.com>
2017-12-27 21:31:46 -0500

    creating ember route 'about'

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -3,10 +3,28 @@ import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptan
 
 moduleForAcceptance('Acceptance | list rentals');
 
-test('visiting /list-rentals', function(assert) {
-  visit('/list-rentals');
 
+test('app renders', function(assert) {
+  visit('/');
   andThen(function() {
-    assert.equal(currentURL(), '/list-rentals');
+    assert.equal(currentURL(), '/');
   });
+});
+
+test('should show rentals as the home page', function (assert) {
+});
+
+test('should link to information about the company.', function (assert) {
+});
+
+test('should link to contact information.', function (assert) {
+});
+
+test('should list available rentals.', function (assert) {
+});
+
+test('should filter the list of rentals by city.', function (assert) {
+});
+
+test('should show details for a selected rental', function (assert) {
 });
