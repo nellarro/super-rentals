ffe01cf45b49b00036ec16da366b1fe7bd8e58d2
Jenell Pizarro <nellarro@gmail.com>
2018-01-01 15:29:21 -0500

    test rental-listing-test

--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -1,24 +1,37 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import EmberObject from '@ember/object';
+import { run } from '@ember/runloop';
+
+let rental = EmberObject.create({
+  image: 'merp.jpg',
+  title: 'Place',
+  owner: 'Human',
+  category: 'apartment',
+  city: 'Winter Park',
+  bedrooms: 3
+});
+
+let mock = this.set('rentalObj', rental);
+let renderMock = this.render(hbs`{{rental-listing rental=rentalObj}}`);
 
 moduleForComponent('rental-listing', 'Integration | Component | rental listing', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('should display rental details', function(assert) {
+  mock;
+  renderMock;
+  assert.equal(this.$('.listing h3').text(), 'Place', 'Title: Place');
+  assert.equal(this.$('.listing .owner').text().trim(), 'Owner: Human', 'Owner: Human');
+});
 
-  this.render(hbs`{{rental-listing}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#rental-listing}}
-      template block text
-    {{/rental-listing}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+test('should toggle wide class on click', function(assert) {
+  mock;
+  renderMock;
+  assert.equal(this.$('.image.wide').length, 0, 'initially rendered small');
+  run(() => document.querySelector('.image').click());
+  assert.equal(this.$('.image.wide').length, 1, 'rendered wide after click');
+  run(() => document.querySelector('.image').click());
+  assert.equal(this.$('.image.wide').length, 0, 'rendered small after second click');
 });
