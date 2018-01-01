5bb0dad41978bc6b2bda87a318c65afc9736dcec
Jenell Pizarro <nellarro@gmail.com>
2018-01-01 14:46:44 -0500

    image appears larger onClick due to the action

--- app/components/rental-listing.js
+++ app/components/rental-listing.js
@@ -1,5 +1,10 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  isWide: false
+  isWide: false,
+  actions:{
+    toggleImageSize() {
+      this.toggleProperty('isWide');
+    }
+  }
 });
